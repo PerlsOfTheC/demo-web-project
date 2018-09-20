d373734d2b96c8d0d08325d328b9345df1595282
Amanda Kwok <amandawkwok@gmail.com>
2018-09-19 21:58:23 -0700

    Added method for Amanda's string

--- src/main/java/edu/csupomona/cs480/controller/WebController.java
+++ src/main/java/edu/csupomona/cs480/controller/WebController.java
@@ -57,6 +57,21 @@ public class WebController {
 	}
 
 	/**
+         * This is a simple example of how the HTTP API works.
+         * It returns a String "whaddup whadduppp" in the HTTP response.
+         * To try it, run the web application locally,
+         * in your web browser, type the link:
+         *      http://localhost:8080/cs480/Amanda
+         */
+        @RequestMapping(value = "/cs480/Amanda", method = RequestMethod.GET)
+        String healthCheck() {
+                // You can replace this with other string,
+                // and run the application locally to check your changes
+                // with the URL: http://localhost:8080/
+                return "whaddup whadduppp";
+        }
+
+	/**
 	 * This is a simple example of how to use a data manager
 	 * to retrieve the data and return it as an HTTP response.
 	 * <p>
